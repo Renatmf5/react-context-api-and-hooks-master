966ba21a1fedf0ca1451643a76f6178adda014b0
Renato Moreno Fereira <renato_mf5@hotmail.com>
2024-01-19 17:15:55 -0300

    Seção 6 full

--- src/contexts/CounterContext/index.jsx
+++ src/contexts/CounterContext/index.jsx
@@ -14,13 +14,12 @@ export const CounterContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const actions = useRef(buildActions(dispatch));
 
-  return <Context.Provider value={[state, actions.current]}>{ children }</Context.Provider>;
+  return <Context.Provider value={[state, actions.current]}>{children}</Context.Provider>;
 };
 
 CounterContextProvider.propTypes = {
   children: P.node.isRequired,
 };
-
 
 export const useCounterContext = () => {
   const context = useContext(Context);
